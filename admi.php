1d986b98d3cf10b6bf5e237f02395384a9d17e72
uSaya24 <95005038+uSaya24@users.noreply.github.com>
2023-05-12 07:28:37 -0500

    Delete admi.php
    nnn

--- admi.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['usuario']) == "" || isset($_SESSION['usuario']) == null) {
-    echo "<script>alert('No tienes permiso para acceder.');
-    window.location.href='./index.php'</script>";
-}
-include_once ('includes/headeradmi.php'); 
-?>
